71fdf3a737298fb274c6bdcd09d945e33e2cd87f
walidZW <iw_zaoui@esi.dz>
2020-03-06 16:46:46 +0100

    pull request update

---
+++ src/kernel/Image.java
@@ -0,0 +1,13 @@
+package kernel;
+//la classe java
+public class Image extends javafx.scene.image.Image {
+
+    public Image(String url) {
+        super(url);
+    }
+
+    public double[] imageToVector(){
+        return null;
+    }
+
+}
---
+++ src/kernel/ImageMat.java
@@ -0,0 +1,13 @@
+package kernel;
+
+public class ImageMat {
+    //les atributs de la classe ImageMat
+  private String Path;
+
+    //le constructeur de la classe ImageMat
+   public ImageMat(String path){
+       this.path=path;
+   }
+
+
+}
